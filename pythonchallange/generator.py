fa2eed2396a78ce5fa79d8c22f1769d16ee29940
Valgard <kamnerobot@gmail.com>
2017-07-03 00:07:33 +0300

    generator add

--- pythonchallange/generator.py
+++ pythonchallange/generator.py
@@ -1,5 +1,6 @@
-res=[x**2 for x in range(1,25,2) if x%3 !=0]
-print(res)
-#test comment
-#test2 comment
-#test3 comment
+# res=[x**2 for x in range(1,25,2) if x%3 !=0]
+# print(res)
+
+x=int(input())
+multtable=[x*i for i in range(1,10)]
+print(multtable)
